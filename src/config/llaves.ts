cdbdaa27d821fea02b1011cdc884ee4e76a0a12a
monicu812 <monicu81@gmail.com>
2022-11-03 17:11:28 -0500

    Agregar carpeta a GIT

---
+++ src/config/llaves.ts
@@ -0,0 +1,4 @@
+export namespace Llaves{
+  export const claveJWT = "MMMR";
+  export const urlServicioNotificaciones = "http//localhost:5000";
+}
---
+++ src/services/autenticacion.service.ts
@@ -0,0 +1,67 @@
+import { /* inject, */ BindingScope, injectable} from '@loopback/core';
+import {repository} from '@loopback/repository';
+import {Llaves} from '../config/llaves';
+import {Usuario} from '../models';
+import {UsuarioRepository} from '../repositories';
+const generador = require('password-generator');
+const cryptoJs = require('crypto-js');
+const jwt = require('jsonwebtoken');
+
+
+@injectable({scope: BindingScope.TRANSIENT})
+export class AutenticacionService {
+  constructor(
+    @repository(UsuarioRepository)
+    public usuarioRepository: UsuarioRepository
+  ) { }
+
+  /*
+   * Add service methods here
+   */
+
+  GenerarContrasena() {
+    let contrasena = generador(8, false);
+    return contrasena;
+  }
+
+  Cifrarcontrasena(contrasena: string) {
+    let contrasenaCifrada = cryptoJs.MD5(contrasena).toString();
+    return contrasenaCifrada
+  }
+
+  IdentificarUsuario(usuario: string, contrasena: string) {
+    try {
+      let p = this.usuarioRepository.findOne({where: {correo: usuario, contrasena: contrasena}});
+      if (p) {
+        return p;
+
+      }
+
+      return false;
+
+    } catch {
+      return false;
+    }
+  }
+
+  GenerarTokenJWT(usuario: Usuario) {
+    let token = jwt.sign({
+      data: {
+        id: usuario.id,
+        correo: usuario.correo,
+        nombre: usuario.nombre
+      }
+    },
+      Llaves.claveJWT);
+    return token;
+  }
+
+  ValidarTokenJWT(token: string) {
+    try {
+      let datos = jwt.verify(token, Llaves.claveJWT);
+      return datos;
+    } catch {
+      return false;
+    }
+  }
+}
